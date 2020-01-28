fe6b3486497f20639c5bd4c4863a61db9beecec5
rchurchASC <rahmelchurch@gmail.com>
2020-01-28 23:16:01 +0000

    start

--- js/game.js
+++ js/game.js
@@ -6,24 +6,24 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You have your first fight next week. How do you prepare?",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Train",
+                    nextLevel: "fightTwo",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Slack off",
+                    nextLevel: "trash",
                 },
             ]
         },
 
-        cave: {
+        trash: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You lost to Glass Joe. You are an embarrassment to boxing. You are absolute garage to the point where you are not allowed to box anymore. YOU SUCK!",
             choices: [
                 {
                     text: "Start over",
@@ -32,15 +32,21 @@ var game = {
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+        fightTwo: {
+            message: "You have defeated your first opponent. What will you do for your next fight?",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Train",
+                    nextLevel: "finalFight",
+                },
+                {
+                    text: "Retire",
+                    nextLevel: "retire1",
                 },
             ]
         },
-
+        retire1: {
+            message: "You retire after defeating Glass Joe saying you suffered brain damage. You are not absolute garbage but you are still bad especially since you got brain damage from Glass Joe. Maybe you were destined for retirement."
+        },
     }
 };
